451d55cfdc5a73055ac318ec72c7a5a1a963cb84
Koos Vriezen <koos.vriezen@gmail.com>
2005-04-07 19:45:25 +0000

    RegionNode has a weak link to parent now, so use it as shortcut for region move animations
    svn path=/trunk/kdeextragear-2/kmplayer/; revision=403868

--- src/kmplayer_smil.cpp
+++ src/kmplayer_smil.cpp
@@ -651,14 +651,17 @@ QString RegionRuntime::setParam (const QString & name, const QString & val) {
         needs_bounds_calc = true;
     }
     if (active && rn && needs_bounds_calc && element) {
-        RegionNodePtr rootrn = element->document ()->rootLayout;
-        if (rootrn && rootrn->regionElement) {
-            convertNode<RegionBase>(rootrn->regionElement)->updateLayout ();
-            QRect nr = rect.unite (QRect (rn->x, rn->y, rn->w, rn->h));
-            PlayListNotify * n = element->document()->notify_listener;
-            if (n)
-                n->repaintRect (nr.x(), nr.y(), nr.width (), nr.height ());
+        if (rn->parentNode ())
+            rn->parentNode ()->calculateChildBounds ();
+        else {
+            RegionNodePtr rootrn = element->document ()->rootLayout;
+            if (rootrn && rootrn->regionElement)
+                convertNode<RegionBase>(rootrn->regionElement)->updateLayout ();
         }
+        QRect nr = rect.unite (QRect (rn->x, rn->y, rn->w, rn->h));
+        PlayListNotify * n = element->document()->notify_listener;
+        if (n)
+            n->repaintRect (nr.x(), nr.y(), nr.width (), nr.height ());
     }
     return ElementRuntime::setParam (name, val);
 }
