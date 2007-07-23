42abb14403a06ec995cf63988f03e00397c1794a
Koos Vriezen <koos.vriezen@gmail.com>
2007-07-23 21:31:12 +0000

    Fix kmplayerapp.cpp:1562: undefined reference to `KMPlayer::PlayListView::updateTree(int, KMPlayer::SharedPtr<KMPlayer::Node>, KMPlayer::SharedPtr<KMPlayer::Node>, bool, bool)'
    svn path=/branches/extragear/kde3/multimedia/kmplayer/; revision=691536

--- src/playlistview.h
+++ src/playlistview.h
@@ -107,7 +107,7 @@ protected:
 public slots:
     void editCurrent ();
     void rename (QListViewItem * item, int c);
-    void updateTree (int id, NodePtr root, NodePtr active, bool sel, bool open) KDE_NO_EXPORT;
+    void updateTree (int id, NodePtr root, NodePtr active, bool sel, bool open);
 private slots:
     void contextMenuItem (QListViewItem *, const QPoint &, int);
     void itemExpanded (QListViewItem *);
