76af6355ef7caa5993fdd3836a0c8b19445e6a14
Koos Vriezen <koos.vriezen@gmail.com>
2009-05-17 15:01:11 +0000

    Atleast add .flv, found on blip.tv a lot
    svn path=/trunk/extragear/multimedia/kmplayer/; revision=969167

--- data/find-media.sh
+++ data/find-media.sh
@@ -2,6 +2,6 @@
 
 echo '<playlist>'
 
-find "$1" -name '*.mp3' -o -name '*.avi' -o -name '*.mp4' -o -name '*.mpg' -o -name '*.ogg' -o -name '*.m3u' | sed 's,^\(.*\)$,  <item url="\1"/>,'
+find "$1" -name '*.mp3' -o -name '*.avi' -o -name '*.mp4' -o -name '*.mpg' -o -name '*.ogg' -o -name '*.m3u' -o -name '*.flv' | sed 's,^\(.*\)$,  <item url="\1"/>,'
 
 echo '</playlist>'
