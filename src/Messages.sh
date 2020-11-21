0da2aba14436ba2e74e0eceeb98e4a42b21c171a
Friedrich W. H. Kossebau <kossebau@kde.org>
2020-11-21 07:25:13 +0100

    Organize src dir into separate subdirs for lib, part, app, backends

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,3 +1,6 @@
 #! /usr/bin/env bash
-$EXTRACTRC *.rc >> rc.cpp
-$XGETTEXT *.cpp -o $podir/kmplayer.pot
+$EXTRACTRC $(find . -name '*.rc') >> rc.cpp || exit 11
+$EXTRACTRC $(find . -name '*.ui') >> rc.cpp || exit 12
+$EXTRACTRC $(find . -name '*.kcfg') >> rc.cpp
+$XGETTEXT lib/*.cpp part/*.cpp app/*.cpp rc.cpp -o $podir/kmplayer.pot
+rm -f rc.cpp
